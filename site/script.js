50b45df59db3c87cf9348d03133daee9ccc40b59
vanecast77 <110347352+vanecast77@users.noreply.github.com>
2022-09-27 09:42:54 -0400

    new change.

--- site/script.js
+++ site/script.js
@@ -1,4 +1,4 @@
-var names=new array();
+var names=new Array();
 names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
@@ -11,9 +11,10 @@ names[8]="Laura";
 names[9]="Jim";
 
 
+
 for (var i =0; i< names.length; i++) {
-  if(names[i].chartAt(0)==='J'|| names[i].chartAt(0)==='j'){
-    console.log("GoodBye" + Snames[i])
+  if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+    console.log("GoodBye" + names[i])
   }
   else {
     console.log("Hello"+names[i])
